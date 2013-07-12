8a604c196d16c40e9a0a0047aeba9e52e59e0040
Justin Kronz <jkronz@gmail.com>
2013-07-12 11:36:33 -0400

    update distribution code

--- dist/app/backbonetree.js
+++ dist/app/backbonetree.js
@@ -1,5 +1,5 @@
 (function() {
-  var _ref, _ref1,
+  var _ref,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -76,6 +76,14 @@
 
   })(Backbone.View);
 
+}).call(this);
+
+(function() {
+  var _ref,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
   backbonetree.TreeNode = (function(_super) {
     __extends(TreeNode, _super);
 
@@ -86,8 +94,8 @@
       this.toggleSelected = __bind(this.toggleSelected, this);
       this.render = __bind(this.render, this);
       this.initialize = __bind(this.initialize, this);
-      _ref1 = TreeNode.__super__.constructor.apply(this, arguments);
-      return _ref1;
+      _ref = TreeNode.__super__.constructor.apply(this, arguments);
+      return _ref;
     }
 
     TreeNode.prototype.tagName = 'li';
